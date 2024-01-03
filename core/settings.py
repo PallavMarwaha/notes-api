a43ff60ff8e6e5dce95087e2a1852559679a608f
Pallav Marwaha <pallav.marwah@gmail.com>
2024-01-04 00:50:06 +0530

    added swagger api docs

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "rest_framework",
     "rest_framework.authtoken",
+    "drf_spectacular",
+    "drf_yasg",
     "authentication",
     "notes",
 ]
@@ -143,4 +145,17 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
     ],
     "DEFAULT_THROTTLE_RATES": {"anon": "20/day", "user": "100/day"},
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
 }
